76bf3d8812fc47e14d5ec4f6e11bf6895ed85c3a
olivebishop <olivehendrilgen1@gmail.com>
2025-11-17 03:48:05 +0300

    fixes

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,6 @@
 import { auth } from "@/lib/auth";
+import { toNextJsHandler } from "better-auth/next-js";
 
-export const GET = auth.handler;
-export const POST = auth.handler;
+const handler = toNextJsHandler(auth);
+
+export { handler as GET, handler as POST };
